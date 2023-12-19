08b66121208a795762b83c60b17565f1e7f5eaee
Arumugam Subramanian <arumugam1987@gmail.com>
2023-12-20 00:10:43 +0530

    feat: groovy scripts development with unit tests

--- src/aru/scripts/version/get_version.groovy
+++ src/aru/scripts/version/get_version.groovy
@@ -30,15 +30,57 @@ def getVersions(String input) {
     return null
 }
 
+def getMajorMinorVersions(String input) {
+    // Regular expression to match version numbers
+    def versionPattern = /[\^~>=<]*([\d]+\.[\d]+)/
+
+    // Find all matches in the input string
+    def matcher = (input =~ versionPattern)
+
+    // Initialize a list to store extracted major.minor versions
+    def majorMinorVersions = []
+
+    // Iterate through matches and extract major.minor version numbers
+    while (matcher.find()) {
+        majorMinorVersions.add(matcher.group(1))
+    }
+
+    // If we have at least two major.minor versions, return them sorted in descending order
+    if (majorMinorVersions.size() >= 2) {
+        majorMinorVersions.sort { a, b -> b <=> a }
+        return majorMinorVersions
+    }
+
+    // If only one major.minor version is found, return it twice
+    if (majorMinorVersions.size() == 1) {
+        return [majorMinorVersions[0]]
+    }
+
+    // Return null if no matches are found
+    return null
+}
+
 // Example usage
 def version1 = getVersions("~3.10")
-def version2 = getVersions(">=3.10,<3.8")
+def version2 = getVersions(">=3.8.1,<3.10.15")
 def version3 = getVersions("^3.10")
-
 def lastVersion = version2.last()
-
 
 binding.variables['version1'] = version1
 binding.variables['version2'] = version2
 binding.variables['version3'] = version3
 binding.variables['lastVersion'] = lastVersion
+
+// Example usage
+def majorMinorVersion1 = getMajorMinorVersions("~3.10")
+def majorMinorVersion2 = getMajorMinorVersions(">=3.8.1,<3.10.15")
+def majorMinorVersion3 = getMajorMinorVersions("^3.10.1")
+def lastMajorMinorVersion = majorMinorVersion2.last()
+
+binding.variables['majorMinorVersion1'] = majorMinorVersion1
+binding.variables['majorMinorVersion2'] = majorMinorVersion2
+binding.variables['majorMinorVersion3'] = majorMinorVersion3
+binding.variables['lastMajorMinorVersion'] = lastMajorMinorVersion
+
+
+
--- test/aru/scripts/ScriptTests.groovy
+++ test/aru/scripts/ScriptTests.groovy
@@ -54,15 +54,30 @@ class ScriptTests {
         def version3 = binding.variables['version3']
         def lastVersion = binding.variables['lastVersion']
 
+        def majorMinorVersion1 = binding.variables['majorMinorVersion1']
+        def majorMinorVersion2 = binding.variables['majorMinorVersion2']
+        def majorMinorVersion3 = binding.variables['majorMinorVersion3']
+        def lastMajorMinorVersion = binding.variables['lastMajorMinorVersion']
+
         assertNotNull version1
         assertNotNull version2
         assertNotNull version3
         assertNotNull lastVersion
 
+        assertNotNull majorMinorVersion1
+        assertNotNull majorMinorVersion2
+        assertNotNull majorMinorVersion3
+        assertNotNull lastMajorMinorVersion
+
         assertEquals (["3.10"], version1)
-        assertEquals (["3.8", "3.10"], version2)
+        assertEquals (["3.8.1", "3.10.15"], version2)
         assertEquals (["3.10"], version3)
-        assertEquals ("3.10", lastVersion)
+        assertEquals ("3.10.15", lastVersion)
+
+        assertEquals (["3.10"], majorMinorVersion1)
+        assertEquals (["3.8", "3.10"], majorMinorVersion2)
+        assertEquals (["3.10"], majorMinorVersion3)
+        assertEquals ("3.10", lastMajorMinorVersion)
     }
 
 }
